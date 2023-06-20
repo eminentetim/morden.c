31e70638cee6974c5f1634093c64c0835409ffd7
eminentetim <etimeminent@gmail.com>
2023-06-20 08:38:37 +0100

    creat a nest loop with for loop

---
+++ nest-for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			printf("27 ");
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	return(0);
+}
