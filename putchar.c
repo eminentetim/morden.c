546bf516d32520c3c21eb1d2fa7dd01388600c0e
eminentetim <etimeminent@gmail.com>
2023-06-24 20:15:38 +0100

    creating a pointer file

---
+++ putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - is void
+ * Return: 0 for success
+ */
+int main(void)
+{
+        char i = '_';
+
+        _putchar(i);
+        i = 'p';
+        _putchar(i);
+        i = 'u';
+        _putchar(i);
+        i = 't';
+        _putchar(i);
+        i = 'c';
+        _putchar(i);
+        i = 'h';
+        _putchar(i);
+        i = 'a';
+        _putchar(i);
+        i = 'r';
+        _putchar(i);
+        _putchar('n');
+        return (0);
+}
+
