69518d037b6668e9fde4225594428663508317f6
eminentetim <etimeminent@gmail.com>
2023-06-27 22:02:18 +0100

    array4

---
+++ array3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*initialization of array in c
+*/
+
+int main(void)
+{
+	int myarr[6] = { 1, 6, 7, 8, 9, 10};
+
+	for (int i = 0; i < 6;  i++)
+	{
+		printf("%d\n", myarr[i]);
+	}
+}
