e4e0e0e5a01d7e7c7caa924ae8582f4dbcc993a6
eminentetim <etimeminent@gmail.com>
2023-07-24 22:05:29 +0100

    exploring differnt characters in c

---
+++ jenny-lecture/char1.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+
+    void main()
+{
+  
+        char b = 98;
+   
+        printf("%c\n", b);
+	printf("%d\n", b);
+}                                                                               
